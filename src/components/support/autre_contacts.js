17b2b69ae95af542357c890f1bd1dffca790d044
RaizoxDev <thierryalexis.dev@gmail.com>
2025-03-18 10:54:43 +0100

    maj

--- src/components/support/autre_contacts.js
+++ src/components/support/autre_contacts.js
@@ -1,12 +1,40 @@
 import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-function Autre_News(){
-    return(
-        <div className="text-center">
-            <h1>Support</h1>
-        </div>
-        
+function Autre_News() {
+    return (
+        <>
+            <div className="text-center">
+                <h1>Support</h1>
+            </div>
+            <div className="text-center" style={{ backgroundColor: "#9D9797" }}>
+                <label>
+                    Pseudo
+                    <input name="username" />
+                </label>
+                <hr />
+                <label>
+                    Email
+                    <input name="email" />
+                </label>
+                <hr />
+                <p>
+                    Problème:
+                    <label>
+                        Problème de Compte
+                        <input type="radio" name="problème" value="Problème de Compte" />
+                    </label>
+                    <label>
+                        Signaler un bug
+                        <input type="radio" name="problème" value="Problème de Launcher" />
+                    </label>
+                    <label>
+                        Problème de Téléchargement
+                        <input type="radio" name="problème" value="Problème de Téléchargement" />
+                    </label>
+                </p>
+            </div>
+        </>
     );
 }
 
