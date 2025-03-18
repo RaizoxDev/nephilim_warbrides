5d0458273e254bc37db09f1fc90da2ab310241f1
RaizoxDev <thierryalexis.dev@gmail.com>
2025-03-18 14:31:02 +0100

    maj

---
+++ src/components/forum/button.js
@@ -0,0 +1,12 @@
+import React, { createElement } from "react";
+
+function Button(){
+    return createElement(
+        <div className="post">
+            Post
+            <img className="pholder" src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADIAAAAyCAYAAAAeP4ixAAAB+ElEQVRoQ+2YQc4BQRCFa65AWDgACxt7ziC4gu2sOQNrlq6A2NqOvS0HsJDMXGH8r5Pq9AjT/k66M6Q6EcZUV1W/79VIRESU/72+fkVykIoxFCIVA0JCRIh4UkCs5UlY57RCxFk6TxuFiCdhndMKEWfpPG0UIp6EdU4rRJyl87Tx94jkefE/iPl8TtfrlRaLBXU6HWcdkyShw+FAy+WyNMeruMvlQuhjv99b62siaZpSvV7XG3A9nU6/+yCj0Yg2m03hICax9XpNcRwTVOz3+5RlmRIB+3a7nRLjdDrRYDBQMY1Gg9rttiIMuu/iQA4LLsBCPIhgvcuLe8ipiZRZy8Q7m81oOBwqu+Adza5WK2q1WtTtdrUV2CqIud/vNJlMiK2CRiEU7Ibv8JlzwgVcDz2Nx2N1MP7OzHs+n5WgWG+thZtQjmeE1We1kQTNo0FesGOtVtPXINfr9fSM8EHQONt4u93S7XbTcWiY75nxZXk/PsjxeNRNo3Cz2SwQYUqw0PODwRzi/xIpm9Pnh8NHRKCS6VFu2KQURVHB+1AJ+9gybKMyzz/PCGYPVjNn5FXeAhHr863iAb/3g1hxwa3tCRGrRIEDhEhgwa3lhIhVosABQiSw4NZyQsQqUeAAIRJYcGs5IWKVKHCAEAksuLWcELFKFDjgZ4g8AIMTiBDSw77UAAAAAElFTkSuQmCC" />
+        </div>
+    );
+}
+
+export default Button;
