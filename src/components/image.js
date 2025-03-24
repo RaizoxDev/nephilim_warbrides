e3255d8efe79470931287ad145d2a4d30dd43e8b
RaizoxDev <thierryalexis.dev@gmail.com>
2025-03-24 09:43:29 +0100

    maj

--- src/components/image.js
+++ src/components/image.js
@@ -1,10 +1,10 @@
 import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import logo from "../components/logo.png";
+import logo from "../../src/components/logo.png";
 
 function Image(){
     return (
-        <div id="DivTotal">
+        <div className="col-12" id="DivTotal">
           <p>Nephilim Warbrides</p>
           <img id="Image" src={logo} alt="Logo" width="250" height="250" />
         </div>
